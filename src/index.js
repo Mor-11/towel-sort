c127a9368738a85ff7105164901a1a16c5ea0f39
Marina <78230561+Mor-11@users.noreply.github.com>
2021-02-16 15:35:44 +0300

    feat: add comments

--- src/index.js
+++ src/index.js
@@ -2,18 +2,18 @@
 // You should implement your task here.
 
 module.exports = function towelSort(matrix) {
-    let nemder = [];
-    if (!matrix) {
+    let nemder = []; // добавление пустого массива 
+    if (!matrix) {  // проверка, если в массив не задан, возрощаем пустой массив 
         return nemder;
-    } else {
-        for (i = 0; i < matrix.length; i++) {
-            if (i % 2 == !0) {
-                for (let j = matrix[i].length - 1; j >= 0; j--) {
-                    nemder.push(matrix[i][j]);
+    } else { 
+        for (i = 0; i < matrix.length; i++) {  //перебор массива 
+            if (i % 2 == !0) { // проверка, если массив в массиве в позиции нечетного  числа
+                for (let j = matrix[i].length - 1; j >= 0; j--) { // перебор массива будет с послелнего 
+                    nemder.push(matrix[i][j]); // добовление  в новый массив из старого массива 
                 }
-            } else {
-                for (let j = 0; j < matrix[i].length; j++) {
-                    nemder.push(matrix[i][j]);
+            } else { //  если массив в массиве в позиции четного  числа
+                for (let j = 0; j < matrix[i].length; j++) {// перебор массива в массиве 
+                    nemder.push(matrix[i][j]);// добовление  в новый массив из старого массива 
                 }
             }
         }
